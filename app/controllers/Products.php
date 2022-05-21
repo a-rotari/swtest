1d29ccbbb751adc6317062403b462a1e3679f2dd
Andrei Rotar <andrei.rotari.work@gmail.com>
2022-05-21 19:07:22 +0300

    Load default view using default controller 'Products'
    This is the most basic implementation of loading a view using default
    controller 'Products'.

--- app/controllers/Products.php
+++ app/controllers/Products.php
@@ -1,8 +1,12 @@
 <?php
 
-class Products
+/**
+ * Controller of the main page of the app. Displays the products.
+ */
+class Products extends Controller
 {
     public function index()
     {
+        $this->view('index');
     }
 }

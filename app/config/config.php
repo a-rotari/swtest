3e8a11b84820e48225d725d69cb1e27e0b0223d5
Andrei Rotar <andrei.rotari.work@gmail.com>
2022-06-04 13:11:06 +0300

    Add config file containing database variables and other variables
    This adds config file with MySQL database variables setting the
    parameters of the database, as well as the URLROOT variable that
    specifies the path to the 'public' directory (containing the index.php
    of the project, images and css file), and the APPROOT directory that
    specifies the path to the 'app' directory (containing libraries, models,
    views and controllers of the app).

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+// Database parameters
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'swtest');
+
+//URL Root
+define('APPROOT', dirname(dirname(__FILE__)));
+define('URLROOT', 'http://sw.test');

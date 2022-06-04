05f4ecc25984efa711d125604cdab8d71e3ec059
Andrei Rotar <andrei.rotari.work@gmail.com>
2022-06-04 15:56:28 +0300

    Refactor constructor method of Core class
    This refactors the constructor method of the Core class to better parse
    URL. See the comment to the constructor method for more details.
    
    This also adds some additional comments.
    
    Any hyphens in URL are now
    removed during URL parsing since the methods are called based on URL,
    and and method names cannot contain hyphens in PHP.

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -8,45 +8,63 @@
 class Core
 {
     // Set default controller, method and params to be called if URL doesn't match
-    protected $currentController = 'Products';
-    protected $currentMethod = 'index';
-    protected $params = [];
+    protected $currentController = 'Products'; // default controller 'Products.php'
+    protected $currentMethod = 'index'; // default method 'index()'
+    protected $params = []; // no parameters as default
 
+    /**
+     * Based on the URL, selects a controller's method with parameters, and calls it.
+     * If URL has a first element, and there is a matching controller file, it is selected as Controller
+     * while the second element is selected as the method.
+     * If there is no matching controller file, the first element is instead selected as default controller's method.
+     * If the default controller has no such method then a default 'index' method is selected, while all remaining
+     * elements are considered parameters.
+     */
     public function __construct()
     {
         // Get the URL in the form of an array
         $url = $this->getUrl();
 
-        // Get controller matching the first element of the array
-        if (isset($url[0]) &&
-            file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
-            $this->currentController = ucwords($url[0]);
-            unset($url[0]);
+        if (isset($url[0])) {
+            if (file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+                $this->currentController = ucwords($url[0]);
+                unset($url[0]);
+                require_once '../app/controllers/' . $this->currentController . '.php';
+                $this->currentController = new $this->currentController;
+                if (isset($url[1]) && method_exists($this->currentController, $url[1])) {
+                    $this->currentMethod = $url[1];
+                    unset($url[1]);
+                }
+            } else {
+                require_once '../app/controllers/' . $this->currentController . '.php';
+                $this->currentController = new $this->currentController;
+                if (method_exists($this->currentController, $url[0])) {
+                    $this->currentMethod = $url[0];
+                    unset($url[0]);
+                }
+            }
+        } else {
+            require_once '../app/controllers/' . $this->currentController . '.php';
+            $this->currentController = new $this->currentController;
         }
-
-        // Require controller and instantiate controller class
-        require_once '../app/controllers/' . $this->currentController . '.php';
-        $this->currentController = new $this->currentController;
-
-        // Get controller's method matching the second element of the array
-        if (isset($url[1]) &&
-            method_exists($this->currentController, $url[1])
-        ) {
-            $this->currentMethod = $url[1];
-            unset($url[1]);
-        }
-
-        // Get any parameters that match the rest of URL's elements
+        // Get any parameters for the method from URL
         $this->params = $url ? array_values($url) : [];
 
         call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
     }
 
+
+    /**
+     * Returns URL exploded into an array
+     * @return array|string[]
+     */
     public function getUrl()
     {
         if (isset($_GET['url'])) {
             $url = rtrim($_GET['url'], '/');
             $url = filter_var($url, FILTER_SANITIZE_URL);
+            // Remove any hyphens from URL since they are not allowed in PHP method names
+            $url = str_replace('-', '', $url);
             return explode('/', $url);
         }
         return [];

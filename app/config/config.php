72645eae58f0a1b67b9870f02f11a868d43789ab
Andrei Rotari <andreichina@gmail.com>
2023-05-26 13:26:08 +0300

    Modify server and mysql name settings

--- app/config/config.php
+++ app/config/config.php
@@ -1,14 +1,15 @@
 <?php
 
 // Database parameters: update them according to your database settings
-define('DB_HOST', 'localhost');
+// define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'root_password');
 define('DB_NAME', 'swtest');
 
 //APP root and URL Root: update them according to your settings
 define('APPROOT', dirname(dirname(__FILE__)));
-define('URLROOT', 'http://sw.test');
+define('URLROOT', 'http://localhost');
 
 //Product types: update according to your products. Keys are used to populate submit form field IDs,
 // values are for text displayed in add product page product selector
